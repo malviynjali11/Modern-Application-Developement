afd78374afd84588cb39663b043c19c7de3be928
malviynjali11 <anjalimalviya061@gmail.com>
2025-06-21 19:33:35 +0530

    Data types

---
+++ Week1/02/datatypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treats all JS code as newer version 
+
+// alert(3+3) // we are using this in browser
+
+console.log(3+3)
+console.log("Anjali")
+
+let name = "Anjali"
+let age = 18
+let isLoggedIn = false
+let state = null
+
+// number 
+// bigint 
+// string => ""
+// boolean => true / false
+// null => standalone value
+// undefined => 
+// symbol => unique
+// object
+
+console.log(typeof "Anjali")
+console.log(typeof 18)
+console.log(typeof false)
+console.log(typeof null)
+console.log(typeof undefined)
