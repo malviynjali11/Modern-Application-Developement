42e0b254c86ffa3ff1d253be6afaa01bfb7083ac
malviynjali11 <anjalimalviya061@gmail.com>
2025-06-22 14:08:14 +0530

    Stack and Heap Memory concepts

---
+++ Week1/05/StackAndHeap.js
@@ -0,0 +1,25 @@
+// **************************************************** STACK and HEAP Memory ****************************************************
+// Stack(Primitive, Primitive value)
+
+// let myYoutubeName = "HiteshChaudhary.com"
+
+// let anotherName = myYoutubeName
+// anotherName = "ChaiAurCode"
+
+// console.log(anotherName)
+// console.log(myYoutubeName)
+
+// Heap(Non-Primitive, Reference Value)
+
+// let userOne = {
+//     email : "user@google.com",
+//     upi : "user@ybl"
+// }
+
+// let userTwo = userOne
+
+// userTwo.email = "hitesh@google.com"
+
+// console.log(userOne.email);
+// console.log(userTwo.email);
+
